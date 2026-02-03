add7ec230deaa3760e591398341efad432532a18
Yashh-Saini <yash.s.code@gmail.com>
2026-02-03 22:04:35 +0530

    for_loop.cpp

---
+++ Day - 2/for_loop.cpp
@@ -0,0 +1,17 @@
+#include<iostream>
+using namespace std;
+
+int main() {
+
+    /* Example-1 */
+
+    for(int i = 1; i <= 5; i++){
+        cout<<"Hello Coder Army"<<endl;
+    }
+
+    /* Example-2 */
+
+    for(int i = 1; i <= 5; i++){
+        cout<<i<<" ";
+    }
+}
